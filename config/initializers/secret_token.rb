f2a21c5e2b4d64e11cc21c195dae70bcfe8da2e3
sameer <sameerdgrea8@gmail.com>
2014-06-18 18:24:02 +0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NsPortal::Application.config.secret_key_base = '6b8f321f2846c63b2e15b74b2cd865c10a5e1541a4e9c8ab9150c30e986599e167ad894e12a6a1fc37fc3657d94f1a60d17437d292a98292ab142cc7e1b2e36f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NsPortal::Application.config.session_store :cookie_store, key: '_ns_portal_session'
